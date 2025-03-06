489bcb998ed32af924c379dcf8c54199c5307a36
egoing <egoing@gmail.com>
2025-03-06 17:18:16 +0900

    서버의 데이터를 변경하는 작업

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,20 @@ import "./App.css";
 import s from "./App.module.css";
 function Counter({ title, initValue }: { title: string; initValue: number }) {
     const [value, setValue] = useState(initValue);
-    const up = () => {
-        // setValue((prev) => prev + 1);
-        setValue(value + 1);
+    const changeHandler = (_value: number) => {
+        const option = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ value: _value }),
+        };
+        fetch("http://localhost:9999/counter", option)
+            .then((resp) => resp.json())
+            .then((result) => setValue(result.value));
     };
-    const down = () => setValue(value - 1);
+    const up = () => changeHandler(value + 1);
+    const down = () => changeHandler(value - 1);
     const style = {
         margin: "5px",
         backgroundColor: "red",
