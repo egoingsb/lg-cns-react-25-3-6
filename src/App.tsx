ffddff6d1861940d11c690b4272353f055b66957
egoing <egoing@gmail.com>
2025-03-06 10:50:13 +0900

    컴포넌트는 사용자 정의 태그다. 컴포넌트는 함수다. 함수 이름은 첫글자가 반드시 대문자다. 함수의 리턴값은 하나의 단일 루트 태그다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+function Counter() {
+    return (
+        <>
+            <h1>카운터</h1>
+            <button>+</button> 0
+        </>
+    );
+}
 
 function App() {
-    return <>Hello world!</>;
+    return (
+        <>
+            <Counter></Counter>
+        </>
+    );
 }
 
 export default App;
