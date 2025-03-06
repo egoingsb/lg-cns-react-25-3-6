1cedf769b6ed8567145328346c50a285414e81d5
egoing <egoing@gmail.com>
2025-03-06 13:30:19 +0900

    사용자와 상호작용할 때는 state가 떠올라야 한다. state는 useState를 통해서 만든다. useState의 파라미터는 초기값이다. state는 배열이다. 첫번째 원소는 읽을 때쓰는 값이고, 두번째 원소는 값을 바꿀 때 쓰는 함수이다.
    onclick이 아니라 onClick이다.
    이벤트로 사용할 값은 반듯시 함수여야 한다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 function Counter({ title, initValue }: { title: string; initValue: number }) {
+    const valueState = useState(initValue);
+    const value = valueState[0];
+    const setValue = valueState[1];
+    function up() {
+        setValue(value + 1);
+    }
     return (
         <>
             <h1>{title}</h1>
-            <button>+</button> {initValue}
+            <button onClick={up}>+</button> {value}
         </>
     );
 }
